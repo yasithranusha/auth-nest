86d21d527367659acaeef14388a74344f5990e22
Yasith Ranusha Silva <yasith.si@raketman.io>
2024-10-31 00:34:41 +0530

    feat: user controller

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dto/user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (user)
+      throw new ConflictException('Account already exists with this email');
+
+    const newUser = await this.prisma.user.create({
+      data: { ...dto, password: await hash(dto.password, 10) },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result } = newUser;
+    return result;
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email: email } });
+  }
+
+  async findById(id: number) {
+    return this.prisma.user.findUnique({ where: { id: id } });
+  }
+}
